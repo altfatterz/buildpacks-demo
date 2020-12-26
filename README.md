169cc6ea6c09b863cba65fe7baae2df6fef7e51c
Zoltan Altfatter <altfatterz@gmail.com>
2020-12-26 10:24:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-
-Resources:
-
-- Building Container Images
-https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-container-images
-- https://paketo.io/docs/
-
-
-```bash
-$ pack build buildpacks-demo --path . --builder cloudfoundry/cnb:bionic 
-```
+### buildpacks-demo
--- src/main/java/com/example/BuildpacksDemoApplication.java
+++ src/main/java/com/example/BuildpacksDemoApplication.java
@@ -2,12 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BuildpacksDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BuildpacksDemoApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String greet() {
+        return "Hello, Buildpacker!";
+    }
+
 }
+
+
